ac370240b91f17bf4b2b3f3ddadd087958b857bd
lineshp <linesh84@gmail.com>
2020-04-18 02:31:07 +0930

    initial commit spring boot

---
+++ src/com/linesh/test/Application.java
@@ -0,0 +1,22 @@
+package com.linesh.test;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan ("com.linesh.*")
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer{
+	
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
