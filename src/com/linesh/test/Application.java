40c3f10e67ff440b6bcd7c78d6170fdfc61cbb0b
Linesh p <linesh84@gmail.com>
2020-04-18 11:35:49 +0930

    test

--- src/com/linesh/test/Application.java
+++ src/com/linesh/test/Application.java
@@ -1,7 +1,4 @@
 package com.linesh.test;
-
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
